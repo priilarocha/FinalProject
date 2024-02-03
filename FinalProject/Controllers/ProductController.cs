a4f3ed5591174a7950f1a48728f3afc30570c1ad
priirocha <prii.rocha@hotmail.com>
2024-02-03 13:07:24 +0000

    Tests with api

---
+++ FinalProject/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FinalProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        Uri baseAddress = new Uri("38b15f82-38b3-459d-9f45-1b917c54e67a.mock.pstmn.io/users");
+        private readonly HttpClient _client;
+
+        public ProductController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<ProductViewModel> productList = new List<ProductViewModel>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/product/Get").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                productList = JsonConvert.DeserializeObject<List<ProductViewModel>>(data);
+            }
+            return View(productList);
+        }
+    }
+}
---
+++ FinalProject/Models/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Qty { get; set; }
+    }
+}
